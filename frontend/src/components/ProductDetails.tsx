a83796570823aa824a14c263ff9e673f2384c7b1
kareem89 <muslimkareem89@icloud.com>
2023-01-25 15:27:59 +0100

    refactored: ProductDetails component

--- frontend/src/components/ProductDetails.tsx
+++ frontend/src/components/ProductDetails.tsx
@@ -3,18 +3,14 @@ import {useParams} from "react-router-dom";
 import {useState} from "react";
 import {IMAGES_PATH} from "../model/aplicationProp";
 import axios from "axios";
+import {Product} from "../model/Product";
 
 export default function ProductDetails() {
-
 
     const {id} = useParams();
     const [product] = useProduct(id as string);
     const [presentPhoto, setPresentPhoto] = useState<string>("")
-    const [activeSidePhoto,setActiveSidePhoto] = useState("")
 
-    const onClick = () => {
-
-    }
 
     let sidePhotos = product.imageURLs.map((img, index) => {
 
@@ -30,32 +26,55 @@ export default function ProductDetails() {
 
 
      const addToCart = async () => {
-        console.log("this is in the Product details "+id)
          const res = await axios.put("/api/orders/"+id)
      }
 
     return (<>
 
 
-            <div className={"details-container"}>
-                <div className={" d-flex justify-content-around flex-wrap "}>
-                    <div>{sidePhotos}</div>
-                    <div className=" present-photo-container border border-5 " style={{width: "25rem"}}>
-                        <img src={presentPhoto ? presentPhoto : IMAGES_PATH+product.imageURLs[0]} className="present-photo " alt={product.imageURLs[0]}/>
+            <div>
+                <div className={" details-container "}>
+                    <div>{product.imageURLs.map((img, index) => {
+
+                            return <div key={index} className={" card  border-5 side-photo "}>
+                                <img
+                                    src={IMAGES_PATH + img}
+                                    onClick={() => {
+                                        setPresentPhoto(IMAGES_PATH + img)
+                                    }} alt="..."/>
+                            </div>
+                        }
+                    )}
                     </div>
+
+                        <img src={presentPhoto ? presentPhoto : IMAGES_PATH + product.imageURLs[0]}
+                             className="present-photo " style={{width: "25rem"}} alt={product.imageURLs[0]}/>
+
+                    <div className={"text-buttons-container"}>
+                        <p className={"description"}>Step into the season in style with our latest collection of
+                            clothing.From flowy dresses to tailored suits, we have something for every occasion." </p>
+                        <div className={"details-button-container"}>
+                            <button className="btn m-t-3 add-to-cart-button" type="button" onClick={addToCart} >In den Warenkorb</button>
+                            <button className="btn buy-button" type="button" >Kaufen</button>
+                        </div>
+                    </div>
+
                 </div>
-            </div>
-            <div className={"details-button-container "}>
-                <div className={"description"}>"Live, Laugh, Love"
-                    "Adventure is out there"
-                    "Life is better with a dog"
 
-                    </div>
 
-                <button className="btn add-to-cart-button" type="button" onClick={addToCart} >In den Warenkorb</button>
-                <button className="btn buy-button" type="button" >Kaufen</button>
+
+
             </div>
+
 
         </>
     )
 }
+
+
+
+
+
+
+
+
