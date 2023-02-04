a6ab79a7f07948c4d91cf19c6b1ce5d03b85e177
kareem89 <muslimkareem89@icloud.com>
2023-02-04 10:31:25 +0100

    refactor addToCart and delete OrderController

--- backend/src/main/java/com/my/shope/backend/product/ProductService.java
+++ backend/src/main/java/com/my/shope/backend/product/ProductService.java
@@ -15,7 +15,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-import static java.lang.System.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,11 +24,13 @@ public class ProductService {
     private final OrderService orderService;
     private final AppUserService userService;
     private final FileService fileService;
+   private final AppUserService appUserService;
+
     public static final String DETAILS_PATH = "/Users/kareem89/IdeaProjects/simple-onlineshope-my-capstone-project/backend/product_details.txt";
     private static final String PRODUCT_DETAILS = "product_details";
 
 
-    public Product createProduct(MultipartFile[] file) throws IOException {
+    public Product createProduct(MultipartFile[] file) throws Exception {
         Product product = new Product();
         List<String> imagesIds = new ArrayList<>();
 
@@ -45,7 +46,6 @@ public class ProductService {
         }
         product.setImageIDs(imagesIds);
 
-        out.println(product);
         return productRepo.save(product);
     }
 
@@ -61,6 +61,32 @@ public class ProductService {
         } else {
             return optionalProduct.get();
         }
+    }
+    public Order addToCart(String productId) {
+        AppUser appUser = appUserService.getAuthenticatedUser();
+
+        Optional<Order> optionOrder = orderService.getOrderByAppUserIdAndIsExcuted(appUser.getId(), false);
+
+        if (optionOrder.isEmpty()) {
+            Order newOrder = new Order(null, appUser.getId(), List.of(productId), false);
+            orderService.save(newOrder);
+            return newOrder;
+        } else {
+
+            boolean isExist = true;
+            for (String pId : optionOrder.get().getProductsIds()) {
+
+                if (productId.equals(pId)) {
+                    isExist = false;
+                    break;
+                }
+            }
+            if (isExist) {
+                optionOrder.get().getProductsIds().add(productId);
+                orderService.save(optionOrder.get());
+            }
+        }
+        return optionOrder.get();
     }
 
 
@@ -121,7 +147,7 @@ public class ProductService {
 
     public void removeProductFromOrder(String productId, Optional<Order> order) {
 
-        if (order.isPresent() && order.get().getProductsIds().size() > 0) {
+        if (order.isPresent() && !order.get().getProductsIds().isEmpty()) {
             for (String pId : order.get().getProductsIds()) {
                 if (productId.equals(pId)) {
                     order.get().getProductsIds().remove(productId);
@@ -133,7 +159,7 @@ public class ProductService {
     }
 
 
-    public void setProductDetails(Product product,String textFilePath) throws IOException {
+    public void setProductDetails(Product product,String textFilePath) throws Exception {
 
         File file = new File(textFilePath) ;
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -150,7 +176,14 @@ public class ProductService {
                     case "price" -> product.setPrice(Double.parseDouble(value));
                     case "category" -> product.setCategory(value);
                     case "id" -> product.setId(value);
-                    default -> br.close();
+                    default -> {
+                        if (value.equals("name")||
+                            value.equals("description")||
+                            value.equals("price")||
+                            value.equals("category")) {
+                            throw new Exception("unknown content of product_details text format");
+                        }
+                    }
                 }
             }
         } catch (IOException e) {
@@ -174,7 +207,7 @@ public class ProductService {
     }
 
 
-    public List<Product> updateProduct(String productId, MultipartFile[] multipartFile) throws IOException {
+    public List<Product> updateProduct(String productId, MultipartFile[] multipartFile) throws Exception {
         Product productToUpdate = getProductById(productId);
 
         // if only product_details in the multipartFile then update only the details
@@ -199,11 +232,10 @@ public class ProductService {
         //just to delete old photos and sett the new photos to mey productToUpdate
         productToUpdate.setImageIDs(new ArrayList<>());
         for (MultipartFile file : multipartFile) {
-            if (!Objects.requireNonNull(file.getOriginalFilename()).startsWith("product_details")) {
+            if (!Objects.requireNonNull(file.getOriginalFilename()).startsWith("PRODUCT_DETAILS")) {
                 productToUpdate.getImageIDs().add(fileService.saveFile(file).getId());
             }
         }
-        out.println(productToUpdate);
          productRepo.save(productToUpdate);
          return getAll();
 
