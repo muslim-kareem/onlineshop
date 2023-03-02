ac566017693757ca85686595c120123b9a314f20
mk <muslimkareem89@icloud.com>
2023-03-02 13:20:07 +0100

    refactor: create PRODUCT_BASE_URL

--- frontend/src/api/ProductApi.ts
+++ frontend/src/api/ProductApi.ts
@@ -1,13 +1,14 @@
 import {Product} from "../model/Product";
 import axios from "axios";
+import {PRODUCT_BASE_URL} from "../model/aplication_properties";
 
 export const getProducts = async (): Promise<Product[]> => {
-    const response = await axios.get<Product[]>('/api/products');
+    const response = await axios.get<Product[]>(PRODUCT_BASE_URL);
     return response.data;
 };
 
 export const getSizeOfShoppingCart= async ()=> {
-    const response = await axios.get('/api/products/shopping-cart-size');
+    const response = await axios.get(PRODUCT_BASE_URL+'/shopping-cart-size');
     return response.data;
 };
 
@@ -17,40 +18,40 @@ export const getProductById = async (productId: string): Promise<Product> => {
 };
 
 export const getProductByCategory = async (category: string): Promise<Product[]> => {
-    const response = await axios.get<Product[]>('/api/products/category/'+category);
+    const response = await axios.get<Product[]>(PRODUCT_BASE_URL+'/category/'+category);
     return response.data;
 };
 
 export const getShoppingCart = async (): Promise<Product[]> => {
-    const response = await axios.get<Product[]>('/api/products/shopping-carts');
+    const response = await axios.get<Product[]>(PRODUCT_BASE_URL+'/shopping-carts');
     return response.data;
 };
 
 export const getOrdered= async (): Promise<Product[]> => {
-    const response = await axios.get<Product[]>('/api/products/ordered');
+    const response = await axios.get<Product[]>(PRODUCT_BASE_URL+'/ordered');
     return response.data;
 };
 
 export const getOrderAll= async ()=> {
-    const response = await axios.get('/api/products/order-all');
+    const response = await axios.get(PRODUCT_BASE_URL+'/order-all');
     return response.data;
 };
 export const removeFromShoppingCart = async (productId: string): Promise<Product> => {
-    const response = await axios.delete('/api/products/shopping-carts/'+productId);
+    const response = await axios.delete(PRODUCT_BASE_URL+'/shopping-carts/'+productId);
     return response.data;
 };
 
 export const removeOrdered = async (productId: string): Promise<Product> => {
-    const response = await axios.delete('/api/products/ordered/'+productId);
+    const response = await axios.delete(PRODUCT_BASE_URL+'/ordered/'+productId);
     return response.data;
 };
 export const deleteProduct = async (productId: string): Promise<Product> => {
-    const response = await axios.delete<Product>('/api/products/'+productId);
+    const response = await axios.delete<Product>(PRODUCT_BASE_URL+'/'+productId);
     return response.data;
 };
 
 
 export const getByTitle = async (name: string): Promise<Product[]> => {
-    const response = await axios.get('/api/products/search-by-name/'+name);
+    const response = await axios.get(PRODUCT_BASE_URL+'/search-by-name/'+name);
     return response.data;
 };
--- frontend/src/model/aplication_properties.ts
+++ frontend/src/model/aplication_properties.ts
@@ -1,3 +1,4 @@
 export const LOCAL_IMAGES_PATH = process.env.PUBLIC_URL + "/images/"
 
 export const IMAGES_PATH = "/api/files/"
+export const PRODUCT_BASE_URL = "/api/products"
